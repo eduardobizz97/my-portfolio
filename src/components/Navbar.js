6f4e8eb418be5782c080110b161b3a7c2628d97a
Eduardo <ebmcangrejo97@gmail.com>
2021-03-20 23:12:54 -0300

    CUstom hambuguer menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { FaBars } from 'react-icons/fa';
+import React, {useEffect, useState} from "react";
 import {
   Link,
   NavLink,
@@ -13,26 +14,38 @@ import AboutMeContainer from "../containers/about-me/AboutMeContainer";
 import ContactContainer from "../containers/contact-me/ContactContainer";
 import HomeContainer from "../containers/home/HomeContainer";
 import ProjectsContainer from "../containers/projects/ProjectsContainer";
-
+import { useWindowSize } from "../utils/useWindowsSize";
 
 const NavContainer = styled.div``;
 
 const BodyContainer = styled.div``;
 
 const NavBar = styled.nav`
+
   height: 70px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: flex-end;
   background-color: ${COLORS.accent};
+
+  
+
+
 `;
 
 const UnorderedList = styled.ul`
+
   display: flex;
   flex-direction: row;
   justify-content: center;
   margin: 0;
+  @media (max-width: 768px) {
+    .link{
+      display: none;
+    }
+  }
+  
 `;
 
 const ListItem = styled.li`
@@ -40,57 +53,86 @@ const ListItem = styled.li`
   display: inline-block;
   margin: 10px;
   font-weight: 400;
+  
+`;
+
+const Icon = styled(FaBars)`
+  display: none;
+  color: white;
+  font-size: 50px;
+  border: 1px solid white;
+  border-radius: 8px;
+  &:hover{
+    cursor: pointer;
+  }
+  @media (max-width: 768px) {
+    display: block;
+  }
+  
 `;
 
 const Navbar = (props) => {
+  const [mobile, setMobile] = useState(false)
+  const { width } = useWindowSize();
 
+  const burguerMenuHandler = ()=>{
+    alert('chupalo');
+  };
+  let NavbarComponent = (
+    <NavBar>
+      <UnorderedList>
+        <ListItem>
+          <NavLink
+            to="/"
+            exact
+            className='link'
+            style={{ color: "white", textDecoration: "none" }}
+            activeStyle={{ color: "#e9c46a" }}
+          >
+            Inicio
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink
+            to="/projectos"
+            className='link'
+            style={{ color: "white", textDecoration: "none" }}
+            activeStyle={{ color: "#e9c46a" }}
+          >
+            Proyectos
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink
+            to="/acerca"
+            className='link'
+            style={{ color: "white", textDecoration: "none" }}
+            activeStyle={{ color: "#e9c46a" }}
+          >
+            Acerca de mi
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink
+            to="/contacto"
+            className='link'
+            style={{ color: "white", textDecoration: "none" }}
+            activeStyle={{ color: "#e9c46a" }}
+          >
+            Contacto
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <Icon onClick={burguerMenuHandler}/>
+        </ListItem>
+      </UnorderedList>
+    </NavBar>
+  );  
 
   return (
     <div>
       <NavContainer>
-        <header>
-          <NavBar>
-            <UnorderedList>
-              <ListItem>
-                <NavLink
-                  to="/"
-                  exact
-                  style={{ color: "white", textDecoration: "none" }}
-                  activeStyle={{ color: "#e9c46a" }}
-                >
-                  Inicio
-                </NavLink>
-              </ListItem>
-              <ListItem>
-                <NavLink
-                  to="/projectos"
-                  style={{ color: "white", textDecoration: "none" }}
-                  activeStyle={{ color: "#e9c46a" }}
-                >
-                  Proyectos
-                </NavLink>
-              </ListItem>
-              <ListItem>
-                <NavLink
-                  to="/acerca"
-                  style={{ color: "white", textDecoration: "none" }}
-                  activeStyle={{ color: "#e9c46a" }}
-                >
-                  Acerca de mi
-                </NavLink>
-              </ListItem>
-              <ListItem>
-                <NavLink
-                  to="/contacto"
-                  style={{ color: "white", textDecoration: "none" }}
-                  activeStyle={{ color: "#e9c46a" }}
-                >
-                  Contacto
-                </NavLink>
-              </ListItem>
-            </UnorderedList>
-          </NavBar>
-        </header>
+        <header>{NavbarComponent}</header>
       </NavContainer>
       <BodyContainer>
         <Switch>
